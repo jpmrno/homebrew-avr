ab29ea5159441f8617d3f103bad292cfce6eb817
Juan <jpmrno@users.noreply.github.com>
2018-08-15 00:29:34 -0300

    fix 'No such file or directory @ dir_chdir' error

--- avarice.rb
+++ avarice.rb
@@ -1,19 +1,31 @@
 class Avarice < Formula
-  homepage "http://avarice.sourceforge.net/"
-  head "https://svn.code.sf.net/p/avarice/code/trunk", :using => :svn
+  desc "Interfaces the GNU GDB with the AVR JTAG ICE from Atmel"
+  homepage "https://avarice.sourceforge.io/"
+  head "https://svn.code.sf.net/p/avarice/code/trunk"
 
   depends_on "binutils"
   depends_on "hidapi"
   depends_on "automake"
 
   def install
-    chdir "avarice" do
-      system "./Bootstrap"
-      system "./configure", "--disable-debug",
-                            "--disable-dependency-tracking",
-                            "--disable-silent-rules",
-                            "--prefix=#{prefix}"
+    args = [
+      "--disable-debug",
+      "--disable-dependency-tracking",
+      "--disable-silent-rules",
+      "--disable-nls",
+      "--prefix=#{prefix}",
+    ]
+
+    system "./Bootstrap"
+
+    mkdir "build" do
+      system "../configure", *args
+      system "make"
       system "make", "install"
     end
+  end
+  
+  test do
+    system "#{bin}/avarice", "--version"
   end
 end
